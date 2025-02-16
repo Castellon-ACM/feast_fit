0251040065d1af529954324c2bb5f145b58ad9cd
Ivan <ipeross2207@g.educaand.es>
2025-02-16 21:45:03 +0100

    Starting HomeScreen. Adding title and some text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,32 @@
 import 'package:feast_fit/widgets/widgets.dart';
 import 'package:flutter/material.dart';
- // Asegúrate de importar SettingsScreen
+// Asegúrate de importar SettingsScreen
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return const Scaffold(
       appBar: CustomAppBar2(title: 'Inicio'),
-      body: Center(child: Text('Hola Mundo')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Bienvenido a FeastFit',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Explora recetas saludables y personaliza tu plan de alimentación.',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   
-  }
+}
 
